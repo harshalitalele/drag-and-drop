d5ebf235784b79fc839bb08b70b141853c0d76a7
harshalitalele <harshalitalele@gmail.com>
2018-06-21 15:39:36 +0530

    sample in action

--- app.js
+++ app.js
@@ -1,1 +1,5 @@
-setDragDrop(elements, "elements-list", "main-canvas");
+//setDragDrop(elements, "elements-list", "main-canvas");
+makeDraggable("elements-list", "main-canvas");
+makeDraggable("btn1", "main-canvas");
+makeDraggable("btn2", "main-canvas");
+makeDraggable("input1", "main-canvas");
--- makeDraggable.js
+++ makeDraggable.js
@@ -1,4 +1,4 @@
-var makeDraggable = function() {
+var makeDraggable = (function() {
     
     /**
      * @param draggable element id, droppable area id
@@ -8,10 +8,23 @@ var makeDraggable = function() {
      * Custom event handlers : https://developer.mozilla.org/en-US/docs/Web/Guide/Events/Creating_and_triggering_events
      */
     return function(dragid, dropid) {
-        //Set dragid element draggable    
-        document.getElementById(dragid).setAttribute("draggable", true);
+        //remove all previously existing drag drop related handlers
+        //Set dragid element draggable
+        var draggableElem = document.getElementById(dragid);
+        draggableElem.setAttribute("draggable", true);
+        var droppableRegion = document.getElementById(dropid);
+        draggableElem.addEventListener("dragstart", function(e) {
+            e.dataTransfer.setData("text", e.target.id);
+        });
+        droppableRegion.addEventListener("dragover", function(e) {
+            e.preventDefault();
+        });
+        droppableRegion.addEventListener("drop", function(e) {
+            e.preventDefault();
+            var data = e.dataTransfer.getData("text");
+            e.target.appendChild(document.getElementById(data));
+        });
         //Set conditions on droppable zone to accept only corresponding draggable elements
         //Add all drag drop events and raise user-friendly event handlers
-        //
     };
-};
+})();
