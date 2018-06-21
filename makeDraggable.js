a73122b80d27708d84a6468121f2f0f8f33a1a5f
harshalitalele <harshalitalele@gmail.com>
2018-06-21 17:07:12 +0530

    _dropdragMap added to avoid multiple drop handlers

--- makeDraggable.js
+++ makeDraggable.js
@@ -1,4 +1,5 @@
 var makeDraggable = (function() {
+    var _dropdragMap = {};
     
     /**
      * @param draggable element id, droppable area id
@@ -12,23 +13,38 @@ var makeDraggable = (function() {
      * Custom event handlers : https://developer.mozilla.org/en-US/docs/Web/Guide/Events/Creating_and_triggering_events
      */
     return function(dragid, dropid) {
-        //remove all previously existing drag drop related handlers
-        //Set dragid element draggable
+        var _dropHandlerExists = false;
+        if(_dropdragMap.hasOwnProperty(dropid)) {
+            _dropHandlerExists = true;
+            if(!Array.isArray(_dropdragMap[dropid])) {
+                console.error("invalid _dropdragMap at " + dropid + " : " + _dropdragMap[dropid]);
+                return;
+            }
+        } else {
+            _dropdragMap[dropid] = [];
+        }
+        
+        /********* dragid settings begin here *********/
+        _dropdragMap[dropid].push(dragid);
         var draggableElem = document.getElementById(dragid);
         draggableElem.setAttribute("draggable", true);
-        var droppableRegion = document.getElementById(dropid);
         draggableElem.addEventListener("dragstart", function(e) {
             e.dataTransfer.setData("text", e.target.id);
         });
-        droppableRegion.addEventListener("dragover", function(e) {
-            e.preventDefault();
-        });
-        droppableRegion.addEventListener("drop", function(e) {
-            e.preventDefault();
-            var data = e.dataTransfer.getData("text");
-            e.target.appendChild(document.getElementById(data));
-        });
-        //Set conditions on droppable zone to accept only corresponding draggable elements
-        //Add all drag drop events and raise user-friendly event handlers
+        /********* dragid settings end here ***********/
+        
+        if(!_dropHandlerExists) {
+            var droppableRegion = document.getElementById(dropid);
+            droppableRegion.addEventListener("dragover", function(e) {
+                e.preventDefault();
+            });
+            droppableRegion.addEventListener("drop", function(e) {
+                e.preventDefault();
+                // To Do:  Check if this is valid drop from _dropdragMap array for this dropid
+                var data = e.dataTransfer.getData("text");
+                e.target.appendChild(document.getElementById(data));
+                console.log("Check: " + JSON.stringify(_dropdragMap));
+            });
+        }
     };
 })();
