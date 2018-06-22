6b7c9a0a69cedfbf03e8548dd68ac5f9193b96a9
harshalitalele <harshalitalele@gmail.com>
2018-06-22 12:42:51 +0530

    updated gulptasks and todos

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,15 @@
+/**
+ * To Do:
+ * 1. Add minification for html and css as well
+ * 2. use proper folder structure for the project
+ */
 var gulp = require('gulp');
 
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 
 gulp.task('lib', function() {
-    gulp.src('./src/*.js')
+    gulp.src('./*.js')
         .pipe(concat('makeDraggable.js'))
         .pipe(uglify())
         .pipe(gulp.dest('./build/'));
